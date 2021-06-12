eb1c8d6499a7661c4af4f716ff46a09432b96775
hggmarks <hggmarks@gmail.com>
2021-06-12 04:30:37 -0300

    adiçao da movimentaçaolateral  basica (sem animaçao), colisao com o chao e gravidade

--- Projeto/main.c
+++ Projeto/main.c
@@ -1,135 +1,142 @@
-/*******************************************************************************************
-*
-*   raylib [textures] example - Background scrolling
-*
-*   This example has been created using raylib 2.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2019 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description: Project: N30 - N Scketch 
+ *
+ *        Version:  1.0
+ *        Created:  11/06/2021 14:00:33
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:
+ *
+ * =====================================================================================
+ */
 
-#include "C:/raylib/src/raylib.h"
+#include "raylib.h"
+#include <stdio.h>
 
-int main(void)
+#define MAX_FRAME_SPEED 15
+#define MIN_FRAME_SPEED 1
+#define GRAVIDADE 700
+#define ALTURA_PERSONAGEM 100
+#define LARGURA_PERSONAGEM 100
+
+
+
+//typedef enum GameScreen {LOGO = 0, TITLE, GAMEPLAY, ENDING} GameScreen;
+
+typedef struct Jogador{
+    Vector2 posicao;
+    Vector2 tamanho;
+    float velocidade;
+    bool direcaoPersonagem;
+    bool colisao;
+} Jogador;
+
+
+//prototipos das funções
+static void UpdateJogador(Jogador *jogador, float delta, Rectangle colisor);
+static void DrawJogador(Jogador *jogador, Texture2D sprite, Rectangle frame);
+
+
+    int
+main ( int argc, char *argv[] )
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "Projeto");
+    //Tamanho da Janela
+    const int screenWidth = 1366;
+    const int screenHeight = 768;
 
-    // NOTE: Be careful, background width must be equal or bigger than screen width
-    // if not, texture should be draw more than two times for scrolling effect
+    const char titulo[14] = "Project: N30-N";
+
+    
+
+    InitWindow(screenWidth, screenHeight, titulo);
+
+    //Inicializando texturas
+        //Personagem
+    Texture2D spritePersonagem = LoadTexture("resources/walk.png");
+    Rectangle frameRec = {0, 0, (float)spritePersonagem.width/16, (float)spritePersonagem.height};
+    Rectangle chao = {0, 750, screenWidth , screenHeight};
+        //Background
     Texture2D background = LoadTexture("resources/cyberpunk_street_background.png");
     Texture2D midground = LoadTexture("resources/cyberpunk_street_midground.png");
     Texture2D foreground = LoadTexture("resources/cyberpunk_street_foreground.png");
 
-    float Background = 0.0f;
-    float Midground = 0.0f;
-    float Foreground = 0.0f;
-
-    //----------------------------------------------------------------------------------------
-
-    Texture2D character = LoadTexture("resources/walk.png");        // Texture loading
-
-
-    //Posição do personagem
-    //----------------------------------------------------------------------------------------
-    float character_x=0.0f;
-    float character_y=330.0f;
-    Vector2 position = { character_x,character_y };
-    //----------------------------------------------------------------------------------------
-
     
-    int move_x = 1;                 //Velocidade de movimento
 
-    Rectangle frameRec = { 0.0f, 0.0f, (float)character.width/34, (float)character.height };
-    int currentFrame = 0;
 
+    //Inicializando Jogador
+    static Jogador jogador;
+    jogador.posicao = (Vector2){screenWidth/2, 0};
+    jogador.velocidade = 300;
+    jogador.tamanho.x = jogador.posicao.x + LARGURA_PERSONAGEM;
+    jogador.tamanho.y = jogador.posicao.y + ALTURA_PERSONAGEM;
+
+    //GameScreen telaAtual = LOGO;
     int framesCounter = 0;
-    int framesSpeed = 8;
+    SetTargetFPS(60);
+    float deltaTime;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    //Loop principal do jogo
+    while(!WindowShouldClose()){
+    deltaTime = GetFrameTime();
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
+    BeginDrawing();
 
-        framesCounter++;
+    ClearBackground(GetColor(0x052c46ff));
 
-        //Andar do personagem
-        //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_RIGHT)){
-            position.x +=move_x;
-            if (framesCounter >= (60/framesSpeed)){
-              framesCounter = 0;
-              currentFrame++;
+    DrawTextureEx(background, (Vector2){0 ,0}, 0, 3, WHITE);
+    DrawTextureEx(midground, (Vector2){0 ,0}, 0, 3, WHITE);
+    DrawTextureEx(foreground, (Vector2){0 ,200}, 0, 3, WHITE);
+    
+    UpdateJogador(&jogador, deltaTime, chao);
+    DrawJogador(&jogador, spritePersonagem, frameRec);
+    //DrawRectangleRec(chao, WHITE);
 
-              if (currentFrame > 16) currentFrame = 1;
 
-              frameRec.x = (float)currentFrame*(float)character.width/34;
-
-            }
-        }
-        else if (IsKeyDown(KEY_LEFT)){
-            position.x -=move_x;
-            if (framesCounter >= (60/framesSpeed)){
-              framesCounter = 0;
-              currentFrame--;
-
-              if (currentFrame > 34 || currentFrame <18) currentFrame = 33;
-
-              frameRec.x = (float)currentFrame*(float)character.width/34;
-
-            }
-        }
-
-        if(IsKeyReleased(KEY_RIGHT)){
-            currentFrame = 0;
-            frameRec.x = (float)currentFrame*(float)character.width/34;
-        }
-        else if(IsKeyReleased(KEY_LEFT)){
-            currentFrame = 17;
-            frameRec.x = (float)currentFrame*(float)character.width/34;
-        }
-        //----------------------------------------------------------------------------------
-        
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-
-            ClearBackground(GetColor(0x052c46ff));
-
-            // Draw background image
-            // NOTE: Texture is scaled twice its size
-            DrawTextureEx(background, (Vector2){ Background, 20 }, 0.0f, 2.0f, WHITE);
-            // Draw midground image
-            DrawTextureEx(midground, (Vector2){ Midground, 20 }, 0.0f, 2.0f, WHITE);
-            // Draw foreground image
-            DrawTextureEx(foreground, (Vector2){ Foreground, 70 }, 0.0f, 2.0f, WHITE);
-            // Draw the character's walk
-            DrawTextureRec(character, frameRec, position, WHITE);
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+    EndDrawing();
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadTexture(background);  // Unload background texture
-    UnloadTexture(midground);   // Unload midground texture
-    UnloadTexture(foreground);  // Unload foreground texture
 
-    UnloadTexture(character);   // Unload character texture
+    UnloadTexture(spritePersonagem);
+    UnloadTexture(midground);
+    UnloadTexture(foreground);
 
-    CloseWindow();              // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow();
+    return 1; 
+}		
+/* ----------  end of function main  ---------- */
 
-    return 0;
+static void UpdateJogador(Jogador *jogador, float delta, Rectangle colisor){
+
+    //gravidade
+    jogador->posicao.y += GRAVIDADE * delta;
+
+    if (IsKeyDown(KEY_RIGHT)){ 
+        jogador->posicao.x += jogador->velocidade * delta;
+        jogador->direcaoPersonagem = 1;
+    }
+    if (IsKeyDown(KEY_LEFT)){
+        jogador->posicao.x -= jogador->velocidade * delta;
+        jogador->direcaoPersonagem = 0;
+    }
+    if (IsKeyDown(KEY_UP)) jogador->posicao.y -= 5 * jogador->velocidade * delta;
+    if (IsKeyDown(KEY_DOWN)) jogador->posicao.y += jogador->velocidade *  delta;
+
+    jogador->tamanho.x = jogador->posicao.x + LARGURA_PERSONAGEM;
+    jogador->tamanho.y = jogador->posicao.y + ALTURA_PERSONAGEM;
+
+    if (CheckCollisionPointRec(jogador->tamanho, colisor)) jogador->posicao.y = colisor.y - ALTURA_PERSONAGEM;
+
 }
+    
+static void DrawJogador(Jogador *jogador, Texture2D sprite, Rectangle frame){
+    jogador->direcaoPersonagem == 1 ? 0 : (frame.width *=-1);
+    DrawTextureRec(sprite, frame, jogador->posicao, WHITE);
+}
+
